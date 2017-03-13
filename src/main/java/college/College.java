c82163501711839af78d8ca1ef817aa74e8b0b44
Simon Roberts <simon@dancingcloudservices.com>
2017-03-13 15:53:08 -0700

    getStudentsByCriterion mechanism

--- src/main/java/college/College.java
+++ src/main/java/college/College.java
@@ -5,6 +5,10 @@ import java.util.Arrays;
 import java.util.Comparator;
 import java.util.List;
 
+interface StudentCriterion {
+    boolean test(Student s);
+}
+
 public class College {
 
     public static Comparator<Student> thenComparing(Comparator<Student> first, Comparator<Student> second) {
@@ -14,6 +18,50 @@ public class College {
             return firstTry;
         };
     }
+    
+    public static Comparator<Student> inverse(Comparator<Student> original) {
+        return (s1, s2) -> original.compare(s2, s1);
+    }
+    
+    public static List<Student> getStudentsByCriterion(List<Student> all, StudentCriterion criterion) {
+        List<Student> rv = new ArrayList<>();
+        for (Student s : all) {
+            if (criterion.test(s)) {
+                rv.add(s);
+            }
+        }
+        return rv;
+    }
+//    public static List<Student> getSmartStudents(List<Student> all, float threshold) {
+//        List<Student> rv = new ArrayList<>();
+//        for (Student s : all) {
+//            if (s.getGpa() > threshold) {
+//                rv.add(s);
+//            }
+//        }
+//        return rv;
+//    }
+//
+//    public static List<Student> getEnthusiasticStudents(List<Student> all, int threshold) {
+//        List<Student> rv = new ArrayList<>();
+//        for (Student s : all) {
+//            if (s.getCourses().size() > threshold) {
+//                rv.add(s);
+//            }
+//        }
+//        return rv;
+//    }
+    
+// NO NO NO!
+//    public static List<Student> getSomewhatSmartStudents(List<Student> all) {
+//        List<Student> rv = new ArrayList<>();
+//        for (Student s : all) {
+//            if (s.getGpa() > 2.8F) {
+//                rv.add(s);
+//            }
+//        }
+//        return rv;
+//    }
     
     public static void showStudents(Iterable<Student> in) {
         for (Student s : in) {
@@ -59,5 +107,12 @@ public class College {
         showStudents(college);
         college.sort(thenComparing(gradeOrder, nameOrder));
         showStudents(college);
+        college.sort(thenComparing(inverse(gradeOrder), nameOrder));
+        showStudents(college);
+        
+//        showStudents(getSmartStudents(college, 2.7F));
+//        showStudents(getEnthusiasticStudents(college, 2));
+        showStudents(getStudentsByCriterion(college, s -> s.getGpa() > 3.0F));
+        showStudents(getStudentsByCriterion(college, s -> s.getCourses().size() >= 3));
     }
 }
